12bb9bad3f6d0f709f34dd85bdb3bf041aa5b71f
YoshiAki <yoshiakiley@gmail.com>
2024-04-09 10:56:38 +0800

    xx

--- run.sh
+++ run.sh
@@ -1,26 +1,30 @@
-paddle() {
+build_paddle() {
   version=${2:-latest}
   docker buildx build --load -t aihub-paddle:$version ./paddle
   docker rm -f aihub-paddle
   docker run -dp 8000:8000 --name aihub-paddle aihub-paddle:$version
 }
 
-openai() {
+build_openai() {
   version=${2:-latest}
-  docker buildx build --load -t aihub-openai:$version ./openai
-  docker rm -f aihub-openai
-  docker run -dp 8001:8000 --name aihub-openai aihub-openai:$version
+  docker buildx build --load -t harbor-pro.iauto360.cn/dengxiaopengtest/aihub-openai:$version ./openai
+  docker push harbor-pro.iauto360.cn/dengxiaopengtest/aihub-openai:$version
 }
+
+version=$2
+if [ -z "$version" ]; then
+  version="latest"
+fi
 
 case $1 in
    # 飞浆平台
-   paddle)
+   b_paddle)
     echo "build aihub paddle"
-    paddle ;;
+    build_paddle ;;
     # openai
-   openai)
+   b_openai)
     echo "build openai"
-    openai ;;
+    build_openai ;;
   
 
    *) echo "Invalid argument '$1'. Please use start, stop, or restart." ;;
